7a91599405d4e8e421de0342bfb00106258fff90
Marco Moldovan <marco.moldovan.1995@gmail.com>
2023-02-16 01:18:56 +0100

    CNN refactor done

--- src/models/components/cnn.py
+++ src/models/components/cnn.py
@@ -1,111 +1,106 @@
 import torch
-import torch.nn.functional as F
 import torch.nn as nn
-from torch.autograd import Variable
 
 
-def conv_block(in_channels: int, out_channels: int, kernel_size: int = 3, stride: int = 1, padding: int = 1,
-               use_BN: bool = True):
-    """
-    Simple convolutional block
-
-    :param in_channels: int
-        number of input channels
-    :param out_channels: int
-        number of output channels
-    :param kernel_size: int
-        kernel size
-    :param stride: int
-        Stride of the convolution
-    :param padding: int
-        padded value
-    :param use_BN: bool
-        if BN is applied
-    :return: conv_block: torch.nn.Module
-        a convolutional block layer
-    """
-    c = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
-    a = nn.ReLU(inplace=False)
-    if use_BN:
-        b = nn.BatchNorm2d(out_channels)
-        return nn.Sequential(c, a, b)
-    else:
-        return nn.Sequential(c, a)
-
-
-class CNN(nn.Module):
-    """
-    The model to optimize
-    """
-
+class CNNBlock(nn.Module):
     def __init__(
         self,
-        input_shape=(1, 28, 28), 
-        n_conv_layers: int = 3,
-        kernel_size: int = 5,
-        batch_norm: bool = False,
-        global_avg_pooling: bool = True,
-        in_channels: int = 1,
-        key_conv: str = 'n_channels_conv_',
-        key_fc: str = 'n_channels_fc_',
-        out_channels: int = 16,
-        dropout_rate: float = 0.2,
-        n_fc_layers: int = 3,
-        num_classes: int = 10
-    ):
-        super(CNN, self).__init__()
-        layers = []
-
-        for i in range(n_conv_layers):
-            padding = (kernel_size - 1) // 2
-            conv_block_0 = conv_block(in_channels, out_channels, kernel_size=kernel_size,
-                                      padding=padding, use_BN=batch_norm)
-            p = nn.MaxPool2d(kernel_size=2, stride=2)
-            layers.extend([conv_block_0, p])
-            in_channels = out_channels
-            out_channels_tmp = key_conv + str((i + 1))
-            out_channels = out_channels_tmp if out_channels_tmp else out_channels * 2
-
-        self.conv_layers = nn.Sequential(*layers)
-        self.pooling = nn.AdaptiveAvgPool2d(1) if global_avg_pooling else nn.Identity()
-        self.output_size = num_classes
-
-        self.fc_layers = nn.ModuleList()
-        n_in = self._get_conv_output(input_shape)
-        key_fc = 'n_channels_fc_'
-        n_out = key_fc + '0'
-        n_out = n_out if n_out else 256
-
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int,
+        padding: int,
+        use_batch_norm: bool = True,
+    ) -> None:
+        super().__init__()
         
-        config = {'n_channels_fc_0': 27,
-                    'n_channels_fc_1': 17,
-                    'n_channels_fc_2': 273}
-        for i in range(n_fc_layers):
-            fc = nn.Linear(int(n_in), int(n_out))
-            self.fc_layers += [fc]
-            n_in = n_out
-            n_out_tmp = key_fc + str((i + 1))
-            n_out = n_out_tmp if n_out_tmp else n_out / 2
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.ReLU(),
+            nn.BatchNorm2d(out_channels) if use_batch_norm else nn.Identity(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.block(x)
+    
+    
+class FeedForwardBlock(nn.Module):
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        use_activation: bool = True,
+        dropout_rate: float = 0.2,
+    ) -> None:
+        super().__init__()
+        
+        self.block = nn.Sequential(
+            nn.Linear(input_size, output_size),
+            nn.ReLU() if use_activation else nn.Identity(),
+            nn.Dropout(dropout_rate),
+        )
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.block(x)
+    
+    
+class CNN(nn.Module):
+    def __init__(
+        self,
+        conv_channels: list = [1, 16, 32, 64],
+        kernel_size: int = 5,
+        stride: int = 1,
+        use_batch_norm: bool = True,
+        fc_channels: list = [128, 64, 10],
+    ) -> None:
+        """_summary_
 
-        self.last_fc = nn.Linear(int(n_in), self.output_size)
-        self.dropout = nn.Dropout(p=dropout_rate)
-
-        self.time_train = 0
-
-    # generate input sample and forward to get shape
-    def _get_conv_output(self, shape):
-        bs = 1
-        input = Variable(torch.rand(bs, *shape))
-        output_feat = self.conv_layers(input)
-        output_feat = self.pooling(output_feat)
-        n_size = output_feat.data.view(bs, -1).size(1)
-        return n_size
-
-    def forward(self, x):
-        x = self.conv_layers(x)
-        x = self.pooling(x)
-        x = x.view(x.size(0), -1)
-        for fc_layer in self.fc_layers:
-            x = self.dropout(F.relu(fc_layer(x)))
-        x = self.last_fc(x)
+        Args:
+            conv_channels (list, optional): _description_. Defaults to [1, 16, 32, 64].
+            kernel_size (int, optional): _description_. Defaults to 5.
+            stride (int, optional): _description_. Defaults to 1.
+            use_batch_norm (bool, optional): _description_. Defaults to True.
+            fc_channels (list, optional): List containing the output dimensions of all fully connected layers. Has to contain at list the given default as those are the finals output classes. Number of fully connected layers is given by length of this list. Defaults to [10].
+        """
+        super().__init__()
+        
+        num_conv_layers = len(conv_channels) - 1
+        conv_blocks = []
+        for i in range(num_conv_layers):
+            padding = (kernel_size - 1) // 2
+            block = CNNBlock(conv_channels[i], conv_channels[i+1], kernel_size, stride, padding, use_batch_norm)
+            conv_blocks.append(block)
+        self.conv = nn.Sequential(*conv_blocks)
+        
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        
+        num_fc_layers = len(fc_channels)
+        fc_blocks = []
+        for i in range(num_fc_layers):
+            # only layer
+            if i == 0 and num_fc_layers == 1:
+                block = FeedForwardBlock(conv_channels[-1], fc_channels[i], use_activation=False)
+            # first layer with others to follow
+            elif i == 0 and num_fc_layers > 1:
+                block = FeedForwardBlock(conv_channels[-1], fc_channels[i])
+            # last layer with others before it
+            elif i == num_fc_layers - 1:
+                block = FeedForwardBlock(fc_channels[i-1], fc_channels[i], use_activation=False)
+            # all other layers
+            else:
+                block = FeedForwardBlock(fc_channels[i-1], fc_channels[i])
+            fc_blocks.append(block)
+        self.fc = nn.Sequential(*fc_blocks)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv(x)
+        x = self.avg_pool(x)
+        x = x.reshape(x.shape[0], -1) # or x.view(x.size(0), -1)
+        x = self.fc(x)
         return x
+        
+
+if __name__ == "__main__":
+    _ = CNN()
+
